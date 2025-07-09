ae53c6d33146efef5d38810505e1234ecc41c13c
SalesCampelo <salescampelodev@gmail.com>
2025-07-08 22:38:24 -0300

    Atualização mensal

--- main.js
+++ main.js
@@ -36,4 +36,27 @@
         setInterval(updateCountdown, 1000); // Atualiza o contador a cada segundo
         updateCountdown(); // Chama a função uma vez ao iniciar
 
+        // FRASES ROLANTES
+         const phrases = [
+            "Porque em cada pedaço de mim, sempre haverá um pedaço de você.",
+            "Eu te amo, em todos os universos",
+            "Eu te Amo mil milhões",
+            "Você é o meu caminho, e sempre será o meu caminho",
+            "Eu só sinto você",
+            "Se precisar de mim, estarei lá",
+            "Você me tornou digno",
+            "Tudo o que eu quero é te ver feliz",
+            "Não importa o que aconteça, eu sempre vou te amar"
+            
+        ];
+
+        let currentSty = 0;
+
+        function updateText() {
+            currentSty = (currentSty + 1) % phrases.length; // Atualiza o índice
+            document.getElementById('text').innerText = phrases[currentSty]; // Atualiza o texto
+        }
+
+        setInterval(updateText, 4000);
+
          
